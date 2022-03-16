4bd6b47dc6820e8fda44176ef06e975976e7d0a1
Ian Christiansen <ianc.songdog@gmail.com>
2022-03-16 14:31:04 -0700

    setup file structure for moviepets, created moviepets table and routes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS moviepets;
+
+CREATE TABLE moviepets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    pet_name TEXT NOT NULL,
+    pet_species TEXT NOT NULL,
+    movie TEXT NOT NULL
+);
